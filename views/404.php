92f0dcb20ce68659eb386e18ac245872b7e16c50
rexjmn <rexmayorga97@gmail.com>
2025-02-28 11:33:52 +0100

    Añadir 404.php y corregir rutas en index.php

---
+++ views/404.php
@@ -0,0 +1,10 @@
+<section class="flex justify-center items-center min-h-screen">
+    <div class="text-center">
+        <h1 class="text-6xl text-red-600 font-bold bg-black/10 backdrop-blur-md p-6 rounded-lg" data-aos="fade-up">
+            Erreur 404<br>Page Non Trouvée
+        </h1>
+        <p class="text-xl text-gray-300 mt-4" data-aos="fade-up" data-aos-delay="200">
+            Oups ! Il semble que cette page n'existe pas. <a href="?page=home" class="text-yellow-500 hover:text-yellow-400">Retour à l'accueil</a>.
+        </p>
+    </div>
+</section>
