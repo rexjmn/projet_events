6a61f5a33f7832248771dfc283a5d25462fa602e
rexjmn <rexmayorga97@gmail.com>
2025-02-28 11:47:58 +0100

    Añadir en and vendor index.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'rexma/project_events',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '600e96b02028bbb75c2f7370e3e759f4d298ccb0',
+        'reference' => '69b46db675679491445f7031b72e51fab5347b89',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'rexma/project_events' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '600e96b02028bbb75c2f7370e3e759f4d298ccb0',
+            'reference' => '69b46db675679491445f7031b72e51fab5347b89',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
